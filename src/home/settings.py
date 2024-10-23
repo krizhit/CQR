93266587f8cb05ab46c9d0c80c90688e67e38593
krizhit <krishitisshit@gmail.com>
2024-10-23 19:08:31 +0530

    initialized django project

--- src/home/settings.py
+++ src/home/settings.py
@@ -25,8 +25,10 @@ SECRET_KEY = "django-insecure-!k+pbjrh@upv96k0(wxhn+6j)%i#=6i)8f_e#d8(0e5p(wld(4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [".railway.app"]  # https://saas.prod.railway.app
+if DEBUG:
+    ALLOWED_HOSTS += ["127.0.0.1", "localhost"]
+# do this change
 
 # Application definition
 
